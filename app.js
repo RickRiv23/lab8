d83452a4f2d6d72c3a3999378fa75e665ac60fda
EC2 Default User <ec2-user@ip-172-31-80-112.ec2.internal>
2019-11-08 09:22:07 +0000

    lab 8

--- app.js
+++ app.js
@@ -1,37 +1,91 @@
 const express = require("express");
 const app = express();
+app.set('view engine', 'html');
 app.engine('html', require('ejs').renderFile);
-app.use(express.static("public"));
+app.use(express.static("assets"));
 
 
 //routes
 app.get("/", function(req, res){
-    res.render("index.ejs");
     
+    //console.log(req.query.q1);
+    // let score = 0;
+    // let f1, f2, f3, f4, f5;
+    // f1 = f2 = f3 = f4 = f5 = "Wrong!";
+    
+    // if (req.query.q1.toLowerCase() == "sacramento") {
+    //     score += 20;
+    //     f1 = "You got it!";
+    // }
+    //res.render("index", {"score": score, "feedback1":f1});
+    
+    res.render("index.ejs");
 } );
 
-app.get("/gradeQuiz", function(req, res){
+
+app.get("/gradeQuiz", function(req,res){
+    
+    //console.log(req.query.q1);
     let score = 0;
-    let f1, f2, f3, f4, f5;
-    f1 = f2 = f3 = f4 = f5 = "Wrong!";
+    let f1, f2, f3, f4, f5, f6, f7, f8;
+    f1 = f2 = f3 = f4 = f5 = f6 = f7 = f8 = "Wrong!";
+    
+    console.log(req.query.q2 );
     
     if (req.query.q1.toLowerCase() == "sacramento") {
-        score += 20;
-        f1 = "You got it!";
+        score += 12.5;
+        f1 = "Right!";
+    }
+    if (req.query.q2 == "mo") {
+        score += 12.5;
+        f2 = "Right!";
+    }
+    if (req.query.q3a=="false" && req.query.q3b=="false"
+     && req.query.q3c=="true" && req.query.q3d=="true" ) {
+        score += 12.5;
+        f3 = "Right!";
+    }
+    if (req.query.q4 == "Rhode Island") {
+        score += 12.5;
+        f4 = "Right!";
+    }
+   if (req.query.q5 == "seal2") {
+        score += 12.5;
+        f5 = "Right!";
+    }
+    if (req.query.q6 == 50) {
+        score += 12.5;
+        f6 = "Right!";
+    }
+   if (req.query.q7 == "US") {
+        score += 12.5;
+        f7 = "Right!";
+    }
+   if (req.query.q8 == "Olympia") {
+        score += 12.5;
+        f8 = "Right!";
     }
     
-    if(req.query.q2 == "mo"){
-        score += 20;
-        f2 = "You got it!";
-    }
     
-    res.send({"score":score, "feedback1":f1, "feedback2":f2});
+    
+    res.send( {
+        "score": score,
+        "fback1":f1,
+        "fback2":f2,
+        "fback3":f3,
+        "fback4":f4,
+        "fback5":f5,
+        "fback6":f6,
+        "fback7":f7,
+        "fback8":f8
+    });
+    
+    
+    
 });
 
 
-
-
 //running server
-app.listen(process.env.PORT || 8080, process.env.IP, function(){
+app.listen(process.env.PORT, process.env.IP, function(){
     console.log("Express server is running...");
 })
